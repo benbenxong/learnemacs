dd4a8e6698fc94b22f2768a0032d687abf06662c
alanzhang <hj_chang@163.com>
2017-08-03 15:30:03 +0800

    add lisp/init-packages.el

--- init.el
+++ init.el
@@ -2,43 +2,27 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; (package-initialize)
+
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(require 'init-packages)
+(setq ring-bell-function 'ignore)
+
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+
 (global-auto-revert-mode t)
 
-(when (>= emacs-major-version 24)
-    (require 'package)
-    (package-initialize)
-    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-    )
-  (require 'cl)
-
-  ;;add whatever packages you want here
-  (defvar zilongshanren/packages '(
-				   company
-				   monokai-theme
-				   hungry-delete
-				   swiper
-				   counsel
-				   smartparens
-				   js2-mode
-				   nodejs-repl
-                        
-                )  "Default packages")
-(setq package-selected-packages zilongshanren/packages)
-
-  (defun zilongshanren/packages-installed-p ()
-    (loop for pkg in zilongshanren/packages
-          when (not (package-installed-p pkg)) do (return nil)
-          finally (return t)))
-
-  (unless (zilongshanren/packages-installed-p)
-    (message "%s" "Refreshing package database...")
-    (package-refresh-contents)
-    (dolist (pkg zilongshanren/packages)
-      (when (not (package-installed-p pkg))
-        (package-install pkg))))
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
 (electric-indent-mode 1)
 (setq inhibit-splash-screen t)
 ;- turn off tool-bar
@@ -49,14 +33,12 @@
 ;- define a function to quickly open your config file.
 
 (global-linum-mode t)
+
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-
-(global-company-mode t)
-(setq-default cursor-type 'bar)
 
 (setq-default cursor-type 'bar)
 
@@ -78,28 +60,12 @@
 
 (load-theme 'monokai t)
 
-(require 'hungry-delete)
-(global-hungry-delete-mode)
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-
 (dolist (charset '(kana han cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font) charset
                       (font-spec :family "Microsoft YaHei" :size 12)))
 
 (require 'smartparens-config)
 ;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
 
 (setq auto-mode-alist
       (append
@@ -116,6 +82,19 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 (setq auto-save-default nil)
+
+(require 'popwin)
+(popwin-mode 1)
+
+  (setq-default abbrev-mode t)
+
+  (define-abbrev-table 'global-abbrev-table '(
+                                              ;; signature
+                                              ("8zl" "zilongshanren")
+                                              ;; microsoft
+					      ("8ms" "Mircsoft")
+                                              
+                                              ))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
---
+++ lisp/init-packages.el
@@ -0,0 +1,47 @@
+  (require 'cl)
+
+  (when (>= emacs-major-version 24)
+    ;;(require 'package)
+  ;;  (package-initialize)
+    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+    )
+
+
+  ;;add whatever packages you want here
+  (defvar zilongshanren/packages '(
+				   company
+				   monokai-theme
+				   hungry-delete
+				   swiper
+				   counsel
+				   smartparens
+				   js2-mode
+				   nodejs-repl
+				   popwin
+                )  "Default packages")
+(setq package-selected-packages zilongshanren/packages)
+
+  (defun zilongshanren/packages-installed-p ()
+    (loop for pkg in zilongshanren/packages
+          when (not (package-installed-p pkg)) do (return nil)
+          finally (return t)))
+
+  (unless (zilongshanren/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg zilongshanren/packages)
+      (when (not (package-installed-p pkg))
+        (package-install pkg))))
+
+(global-company-mode t)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+(smartparens-global-mode t)
+
+(provide 'init-packages)
