60424a573e69f4a37b88731ea3437f50c349711a
Alan Zhang <hj_chang@163.com>
2017-08-06 18:16:00 +0800

    day04:03-dired-mode

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -65,5 +65,24 @@
 					try-expand-line
 					try-complete-lisp-symbol-partially
 					try-complete-lisp-symbol))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; 主动加载 Dired Mode
+;; (require 'dired)
+;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+
+;; 延迟加载
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+
+(require 'dired-x)
+
+(setq dired-dwim-target t)
 (provide 'init-better-defaults)
 
--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -21,4 +21,3 @@
 
 
 (provide 'init-keybindings)
-~/.emacs.d/
