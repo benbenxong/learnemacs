e3d5921411a39cec2ee915dcf2f0aa6ff30995a9
alanzhang <hj_chang@163.com>
2017-08-18 15:22:40 +0800

    day06-01-fix-coding-system

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,4 +4,12 @@
 (setq org-agenda-files '("~/org"))
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+(setq org-capture-templates
+        '(("t" "Todo" entry (file+headline "~/.emacs.d/gtd.org" "工作安排")
+           "* TODO [#B] %?\n  %i\n"
+           :empty-lines 1)))
+
+;;r aka remember
+(global-set-key (kbd "C-c r") 'org-capture)
+
 (provide 'init-org)
